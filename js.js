797c188c256d28d463ea8cbd78fb0b6386a13d2e
Patrickhenrique1215 <patrick.henrique.1215@gmail.com>
2026-04-04 14:05:30 -0300

    algumas melhorias

--- js.js
+++ js.js
@@ -1,11 +1,32 @@
-const elements = document.querySelectorAll('.fade-in');
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    }
-  });
-}, { threshold: 0.2 });
+//LINKS DOS CARDS
 
-elements.forEach(el => observer.observe(el));
+document.querySelector('#projeto1').addEventListener('click', function() {
+  window.open('https://karen-s-boutique.vercel.app/index.html', '_blank');
+});
+
+document.querySelector('#projeto2').addEventListener('click', function() {
+  window.open('https://tabela-flash.vercel.app', '_blank');
+});
+
+document.querySelector('#projeto3').addEventListener('click', function() {
+  window.open('https://animaverso-three.vercel.app', '_blank');
+});
+
+
+//EFEITO DO CURSOR
+const glow = document.querySelector('.cursor-glow');
+
+document.addEventListener('mousemove', (e) => {
+    glow.style.transform = `translate(${e.clientX - 20}px, ${e.clientY - 20}px)`;
+});
+
+// Efeito quando clica
+document.addEventListener('mousedown', () => {
+    glow.style.transform += ' scale(0.8)';
+});
+
+document.addEventListener('mouseup', () => {
+    glow.style.transform = glow.style.transform.replace(' scale(0.8)', '');
+});
+
